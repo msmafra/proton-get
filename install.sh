6aa00e2f76ede1fbfa6b8edf57aecfe2558b2a3d
Marcelo dos Santos Mafra <msmafra@gmail.com>
2021-06-13 21:04:55 -0300

    Non-safe remote install test

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+install() {
+    
+if [[ "${user_id}" -ne 0 ]];then
+
+    local CURRENT_USER_BIN
+    local user_id
+    local user_name
+    local script_mode
+
+    CURRENT_USER_BIN="${HOME}/.local/bin"
+    user_id="$(id --user)"
+    user_name="$(id --user --name)"
+    script_mode="740"
+
+    cd "${CURRENT_USER_BIN}" && pwd
+    curl -fsSL https://gitlab.com/msmafra/proton-get/-/raw/master/proton-get >> proton-get
+    #wget https://gitlab.com/msmafra/proton-get/-/raw/master/proton-get -O - >> proton-get
+    install --verbose -D -C --mode="${script_mode}" --owner="${user_name}" --group="${user_name}" ./proton-get --target-directory="${CURRENT_USER_BIN}"
+
+fi
+
+}
+install
