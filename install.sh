dbb1e896d5fcad3981d4c06b6ca8e8018c66d202
Marcelo dos Santos Mafra <msmafra@gmail.com>
2021-06-13 21:14:24 -0300

    fix 5

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ if [[ "${user_id}" -ne 0 ]];then
     cd "${CURRENT_USER_BIN}" && pwd
     curl -sSL https://gitlab.com/msmafra/proton-get/-/raw/master/proton-get >> proton-get
     #wget https://gitlab.com/msmafra/proton-get/-/raw/master/proton-get -O - >> proton-get
+    cd "${CURRENT_USER_BIN}" && pwd
     install --verbose -D -C --mode="${script_mode}" --owner="${user_name}" --group="${user_name}" "${CURRENT_USER_BIN}"/proton-get --target-directory="${CURRENT_USER_BIN}"
     exit 0
 
